4d2bef0c9ec2808e4deaf74489afcdf2a4156035
magdalena.ochman <magdalena.ochman@autotrader.co.uk>
2017-03-29 14:58:18 +0100

    displays the table and does the CORRECT equation

--- src/main/java/heartrate/HeartRateCalculator.java
+++ src/main/java/heartrate/HeartRateCalculator.java
@@ -3,44 +3,24 @@ package heartrate;
 import java.util.Scanner;
 
 public class HeartRateCalculator {
+        public static void main(String[] args) {
+            HeartRateCalculator calculator = new HeartRateCalculator();
+            calculator.run();
+        }
 
-    private int intensity;
-    private int age;
-    private int restingHeartRate;
-
-    public static void main(String[] args) {
-        HeartRateCalculator calculator = new HeartRateCalculator();
-        calculator.run();
-    }
-
-    private void run() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter resting heart rate: ");
-        restingHeartRate = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter age: ");
-        age = Integer.parseInt(scanner.nextLine());
-
-//        if (restingHeartRate == null) {
-//            System.out.printf("Please enter resting heart rate");
-//        }
-//        if(age == null) {
-//            System.out.printf("Please enter your age");
-//        }
-
-        System.out.printf("Age: %s, Resting heart rate: %s\n", age, restingHeartRate);
-        int targetHeartRate = (((220 - age)) - restingHeartRate * intensity + restingHeartRate);
-        for (int intensity = 55; intensity <= 56; intensity += 5) {
+        private void run() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter resting heart rate: ");
+            int restingHeartRate = Integer.parseInt(scanner.nextLine());
+            System.out.print("Enter age: ");
+            int age = Integer.parseInt(scanner.nextLine());
+            System.out.printf("Age: %s, Resting heart rate: %s\n", age, restingHeartRate);
             System.out.printf("Intensity|Heart rate\n");
             System.out.println("---------|----------");
-            System.out.println("   " + intensity + "%" + "   " + "|" + "   " + targetHeartRate);
+
+            for (int intensity = 55; intensity <= 95; intensity += 5) {
+                int targetHeartRate = ((((220 - age) - restingHeartRate) * intensity) / 100) + restingHeartRate;
+                System.out.println("   " + intensity + "%" + "   " + "|" + "   " + targetHeartRate);
+            }
         }
     }
-
-//    public int calculate(int age, int restingHeartRate, int intensity) {
-//
-////        System.out.println(targetHeartRate);
-//        return targetHeartRate;
-//    }
-
-
-}
